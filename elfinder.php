bb0d93b0a0c5a649a22557b0a4d69b1282440a43
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2016-04-17 15:20:15 +0000

    Update meta info

--- elfinder.php
+++ elfinder.php
@@ -1,24 +1,24 @@
 <?php
 
-# get correct id for plugin
+// get correct id for plugin
 define('ELF_ID', basename(__FILE__, '.php'));
 
-# register plugin
+// register plugin
 register_plugin(
-  ELF_ID, //Plugin id
-  'elfinder',   //Plugin name
-  '1.0',     //Plugin version
-  'Lawrence Okoth-Odida',  //Plugin author
-  'http://github.com/lokothodida/', //author website
-  'Provides elFinder File Manager', //Plugin description
-  'files', //page type - on which admin tab to display
-  'elfinder_admin'  //main function (administration)
+  ELF_ID,
+  'elFinder File Manager',
+  '0.1',
+  'Lawrence Okoth-Odida',
+  'http://github.com/lokothodida/',
+  'Provides the elFinder File Manager',
+  'files',
+  'elfinder_admin'
 );
 
-# add a link in the admin tab 'theme'
+// add sidebar link
 add_action('files-sidebar', 'createSideMenu', array(ELF_ID, 'elFinder'));
 
-# functions
+// admin function
 function elfinder_admin() {
   $url = $GLOBALS['SITEURL'] . '/plugins/' . ELF_ID . '/';
 
@@ -30,17 +30,14 @@ function elfinder_admin() {
   <h3>elFinder File Manager</h3>
 
   <link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/smoothness/jquery-ui.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
-
   <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>css/elfinder.min.css">
   <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>css/theme.css">
 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
   <script src="<?php echo $url; ?>js/elfinder.full.js"></script>
 
   <script type="text/javascript" charset="utf-8">
-    // Documentation for client options:
-    // https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
     $(document).ready(function() {
       var config = <?php echo json_encode($elfinder); ?>;
       $('#elfinder').elfinder(config);
