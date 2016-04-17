afef600de4119751dd6bff9134bd2acead573d97
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2016-04-17 15:14:45 +0000

    Tidy up code and update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# gs-elfinder
-elfinder File Manager integration for GetSimple CMS
+# GetSimple elFinder File Manager Plugin
+Integrates an alternative file manager, elFinder, into GetSimple.
+
+# Installation
+* Download and unzip the plugin to the `/plugins` folder.
+* Go to the **Plugins** tab and enable `elfinder`.
+* Go to the **Files** tab and click **elFinder** to use the plugin.
+
+# Credit and Special Thanks
+* [elFinder](https://github.com/Studio-42/elFinder) for the file manager.
--- elfinder.php
+++ elfinder.php
@@ -1,61 +1,52 @@
 <?php
 
 # get correct id for plugin
-$thisfile=basename(__FILE__, ".php");
- 
+define('ELF_ID', basename(__FILE__, '.php'));
+
 # register plugin
 register_plugin(
-  $thisfile, //Plugin id
+  ELF_ID, //Plugin id
   'elfinder',   //Plugin name
   '1.0',     //Plugin version
-  'Chris Cagle',  //Plugin author
-  'http://www.cagintranet.com/', //author website
-  'Finds email addresses in content and components and "hides" them', //Plugin description
+  'Lawrence Okoth-Odida',  //Plugin author
+  'http://github.com/lokothodida/', //author website
+  'Provides elFinder File Manager', //Plugin description
   'files', //page type - on which admin tab to display
   'elfinder_admin'  //main function (administration)
 );
 
 # add a link in the admin tab 'theme'
-add_action('files-sidebar','createSideMenu',array($thisfile,'elfinder'));
+add_action('files-sidebar', 'createSideMenu', array(ELF_ID, 'elFinder'));
 
 # functions
-
- 
 function elfinder_admin() {
-  $id = basename(__FILE__, ".php");
-  $url = $GLOBALS['SITEURL'] . '/plugins/' . $id . '/';
-?>
+  $url = $GLOBALS['SITEURL'] . '/plugins/' . ELF_ID . '/';
 
-<h3>elFinder File Manager</h3>
-<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/smoothness/jquery-ui.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+  // elFinder configuration
+  $elfinder = array(
+    'url' => $url . 'php/allow/connector.minimal.php',
+  );
+  ?>
+  <h3>elFinder File Manager</h3>
 
-    <!-- elFinder CSS (REQUIRED) -->
-    <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>css/elfinder.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>css/theme.css">
+  <link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/smoothness/jquery-ui.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
 
-    <!-- elFinder JS (REQUIRED) -->
-    <script src="<?php echo $url; ?>js/elfinder.full.js"></script>
+  <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>css/elfinder.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>css/theme.css">
 
-    <!-- elFinder initialization (REQUIRED) -->
-    
-    <script type="text/javascript" charset="utf-8">
-      // Documentation for client options:
-      // https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
-      $(document).ready(function() {
-        $('#elfinder').elfinder({
-          url : <?php echo json_encode($url); ?> + 'php/allow/connector.minimal.php',
-        });
-      });
-    </script>
-    
-    
-    <script type="text/javascript" src="../../elfinder-2.0-rc1/js/init.js"></script>
-  </head>
-  <body>
+  <script src="<?php echo $url; ?>js/elfinder.full.js"></script>
 
-    <!-- Element where elFinder will be created (REQUIRED) -->
-    <div id="elfinder"></div>
-<?php
+  <script type="text/javascript" charset="utf-8">
+    // Documentation for client options:
+    // https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
+    $(document).ready(function() {
+      var config = <?php echo json_encode($elfinder); ?>;
+      $('#elfinder').elfinder(config);
+    });
+  </script>
+
+  <div id="elfinder"></div>
+  <?php
 }
